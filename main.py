97acae0b0e0c4f3167e8595aef3285f0d2bbcab3
soulampsito78 <soulampsito@gmail.com>
2026-03-08 18:09:13 +0900

    Update main.py
    웨더키 안정화

--- main.py
+++ main.py
@@ -29,7 +29,13 @@ SUPPORTED_MODES = ["today_genie", "tomorrow_genie"]
 # WEATHER API (사용자가 입력 필요)
 # --------------------------------------------------
 
-WEATHER_API_KEY = "657ae8a89b6e8db019070e95e4305d75"
+# --------------------------------------------------
+# WEATHER API
+# --------------------------------------------------
+
+import os
+
+WEATHER_API_KEY = os.environ.get("657ae8a89b6e8db019070e95e4305d75")
 WEATHER_CITY = "Seoul"
 
 # --------------------------------------------------
